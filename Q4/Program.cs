6b256fe6f7e36c8857cbcad241aef3ff84bac783
Tanvir <Tanvir@LaptopAs>
2021-03-29 15:58:32 +0600

    Add project files.

---
+++ Q4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World4!");
+        }
+    }
+}
---
+++ Q5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlds!");
+        }
+    }
+}
